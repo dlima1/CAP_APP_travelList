e1a010dcf64934b715480cc6900b782548eb1943
dlima1 <dimitrilimac@gmail.com>
2021-09-01 18:29:21 +0000

    corrected travel-list

--- bookinganalysis/webapp/Component.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) {
-    return AppComponent.extend("sap.fe.demo.bookinganalysis.Component", {
-        metadata: {
-            manifest: "json"
-        }
-    });
-});
-
-// comment
